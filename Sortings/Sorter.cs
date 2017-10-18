f54b60755346015cb15eef1c0909d0735ac50eec
Dmitriy Astapchik <32869697+DmitriyAstapchik@users.noreply.github.com>
2017-10-18 18:01:47 +0300

    Add files via upload

---
+++ Sortings/Sorter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Sortings
+{
+    public static class Sorter
+    {
+        static Random rand = new Random();
+
+        public static void Quicksort(int[] array)
+        {
+            Partition(array, 0, array.Length - 1);
+        }
+
+        public static int[] MergeSort(int[] array)
+        {
+            int[][] subArrays = Divide(array);
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (subArrays[i].Length > 1)
+                {
+                    subArrays[i] = MergeSort(subArrays[i]);
+                }
+            }
+
+            return Merge(subArrays[0], subArrays[1]);
+        }
+
+        static void Partition(int[] array, int firstIndex, int lastIndex)
+        {
+            int lowIndex = firstIndex; // current index for low number
+            int highIndex = lastIndex; // current index for high number
+            var pivot = array[rand.Next(firstIndex, lastIndex + 1)];
+
+            for (int i = firstIndex; i <= highIndex; i++)
+            {
+                if (array[i] < pivot)
+                {
+                    Swap(ref array[i], ref array[lowIndex++]);
+                }
+
+                if (array[i] > pivot)
+                {
+                    Swap(ref array[i--], ref array[highIndex--]);
+                }
+            }
+
+            if (lowIndex - 1 > firstIndex)
+            {
+                Partition(array, firstIndex, lowIndex - 1);
+            }
+
+            if (highIndex + 1 < lastIndex)
+            {
+                Partition(array, highIndex + 1, lastIndex);
+            }
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            if (a != b)
+            {
+                var temp = a;
+                a = b;
+                b = temp;
+            }
+        }
+
+        static int[][] Divide(int[] array)
+        {
+            var leftPart = new int[array.Length / 2];
+            var rightPart = new int[array.Length - leftPart.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < leftPart.Length)
+                {
+                    leftPart[i] = array[i];
+                }
+                else
+                {
+                    rightPart[i - leftPart.Length] = array[i];
+                }
+            }
+
+            return new int[2][] { leftPart, rightPart };
+        }
+
+        static int[] Merge(int[] leftPart, int[] rightPart)
+        {
+            int[] result = new int[leftPart.Length + rightPart.Length];
+
+            for (int i = 0, li = 0, ri = 0; i < result.Length; i++)
+            {
+                if (li >= leftPart.Length)
+                {
+                    result[i] = rightPart[ri++];
+                }
+                else if (ri >= rightPart.Length)
+                {
+                    result[i] = leftPart[li++];
+                }
+                else if (leftPart[li] < rightPart[ri])
+                {
+                    result[i] = leftPart[li++];
+                }
+                else
+                {
+                    result[i] = rightPart[ri++];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SortingsTest/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Sortings;
+
+namespace SortingsTest
+{
+    class Program
+    {
+        static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            TestQuicksort(200);
+            TestMergeSort(200);
+
+            Console.Read();
+        }
+
+        static bool IsSorted(int[] array)
+        {
+            var sorted = true;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    sorted = false;
+                }
+            }
+
+            return sorted;
+        }
+
+        static int[] MakeArray()
+        {
+            var array = new int[rand.Next(10, 100)];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next();
+            }
+
+            return array;
+        }
+
+        static void TestQuicksort(byte times)
+        {
+            var succeeded = 0;
+
+            for (int i = 0; i < times; i++)
+            {
+                var array = MakeArray();
+                Sorter.Quicksort(array);
+                if (IsSorted(array))
+                {
+                    succeeded++;
+                }
+            }
+
+            Console.WriteLine($"Quicksort test. Succeeded: {succeeded} of {times}.");
+        }
+
+        static void TestMergeSort(byte times)
+        {
+            var succeeded = 0;
+
+            for (int i = 0; i < times; i++)
+            {
+                var array = MakeArray();
+                if (IsSorted(Sorter.MergeSort(array)))
+                {
+                    succeeded++;
+                }
+            }
+
+            Console.WriteLine($"Merge sort test. Succeeded: {succeeded} of {times}.");
+        }
+    }
+}
